73016670e1f57a1fe5f9056542cc601e3634f165
Steven L. Speek <slspeek@gmail.com>
2015-04-14 20:23:23 +0200

    Gae-installer with Shotwell option

--- installer-gae/src/izpack/peer.sh
+++ installer-gae/src/izpack/peer.sh
@@ -3,6 +3,7 @@ java -Dpause=30 \
      -Djava.util.logging.config.file=logging.properties \
      -Dendpoint=${application.id}.appspot.com \
      -Dbot.secret=$bot.secret.userinput \
+     -Dshotwell=$shotwell.userinput \
      -Ddb=$photos.db.userinput  \
      -cp $INSTALL_PATH/peer-jar-with-dependencies.jar \
      com.googlecode.fspotcloud.peer.Main
